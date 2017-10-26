1f0afa0c7edc8d3fdd76b0229c47650e02985eeb
lav <lobanov.andrey.lavich@gmail.com>
2017-10-26 13:31:25 +0500

    ADD handle select

--- static/js/multicooker.js
+++ static/js/multicooker.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-    $.getJSON( "/api/recipes", function( data ) {
-        var items = [];
-        $.each( data.recipes, function( key, val ) {
-            $('#select-recipes').append('<option>'+val.name+'</option>');
-        });     
-    });
+  $.getJSON( "/api/recipes", function( data ) {
+      var items = [];
+      $.each( data.recipes, function( key, val ) {
+          $('#select-recipes').append('<option value="' + val.url + '">'+val.name+'</option>');
+      });     
+  });
 });
+
+function handleSelect(elm)
+{
+  window.location = elm.value;
+}
