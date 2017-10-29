d51e64df7f7abc0f38ad65b52ad568030dd358b3
lav <lobanov.andrey.lavich@gmail.com>
2017-10-29 23:27:08 +0500

    move host and port to DBApp.init()

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import gc
 import picoweb
+import sys
+
 import models
 
 
@@ -8,6 +10,13 @@ class DBApp(picoweb.WebApp):
     def init(self):
         models.db.connect()
         super().init()
+        app.debug = False
+        if sys.platform == 'esp8266': 
+	        self.host='192.168.43.53'
+	        self.port=80
+	    else:
+	        self.host="127.0.0.1"
+	        self.port=8081
 
 
 app = DBApp('__main__')
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import gc
-import sys
 import uasyncio as asyncio
 
 from app import app
 import views
 
-host="127.0.0.1"
-port=8081
 
-
-async def toggle():
+async def print42():
     while True:
         await asyncio.sleep_ms(1000)
         print(42)
@@ -19,23 +15,21 @@ def main(**params):
     gc.collect()
     import logging
     logging.basicConfig(level=logging.INFO)
-
     gc.collect()
 
     import micropython
     micropython.mem_info()
 
-    if sys.platform == 'esp8266': 
-        app.run(debug=True, host='192.168.43.53', port=80, **params)
-    else:
-        app.init()
-        app.debug = True    
-        print("* Running on http://%s:%s/" % (host, port))
-        loop = asyncio.get_event_loop()
-        loop.create_task(asyncio.start_server(app._handle, host, port))
-        loop.create_task(toggle())
-        loop.run_forever()
-        loop.close()
+
+    app.init()
+    app.debug = True    
+    print("* Running on http://%s:%s/" % (app.host, app.port))
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(asyncio.start_server(app._handle, app.host, app.port))
+    loop.create_task(print42())
+    loop.run_forever()
+    loop.close()
 
 
 if __name__ == '__main__':
